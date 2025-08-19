02437ed45ccbb34e7610efa03d942c9ddf1fde8d
Ayesha Jahangir <ayeshajahangir280@gmail.com>
2025-08-20 03:06:06 +0500

    runtime

--- setup.py
+++ setup.py
@@ -4,15 +4,6 @@ IMS Backend Project Setup Script
 This script sets up the Django backend for the Inventory Management System
 Note: This is a project setup script, not a package setup file.
 """
-def get_version():
-def run_command(command, description=""):
-def create_env_file():
-def create_superuser():
-def collect_static():
-def install_requirements():
-def setup_redis():
-def setup_ocr():
-def main():
 
 from setuptools import setup, find_packages
 
